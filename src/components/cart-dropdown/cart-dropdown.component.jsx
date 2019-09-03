4a029b42d6126c2e494b62eebd22c3dbdf98ce9f
TrymH <trym.helle@bouvet.no>
2019-09-03 13:57:06 +0200

    Oppretter cart-item komponent og kobler cart dropdown til cart-reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./cart-dropdown.styles.scss";
+import { connect } from "react-redux";
 
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+// det sånn vi må extracte objekter i cart reducer, når vi har en root reducer med cart som en av disse reducerene
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
