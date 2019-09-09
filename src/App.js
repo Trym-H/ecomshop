2ce474096dbe123feaf35181073e23b5f0bca1c6
TrymH <trym.helle@bouvet.no>
2019-09-09 13:49:48 +0200

    legger til contact-info side.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
+import ContactPage from "./pages/contact/contact.component";
 
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
@@ -62,6 +63,7 @@ class App extends React.Component {
               )
             }
           />
+          <Route path="/contact" component={ContactPage} />
         </Switch>
       </div>
     );
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./contact.styles.scss";
+
+const ContactPage = () => (
+  <div className="contact-container">
+    <h1>Contact Us:</h1>
+    <p>You can contact our staff here: example@example.com</p>
+    <p>Or reach out at phone number: +44 987 65 432</p>
+    <p>More info...</p>
+  </div>
+);
+
+export default ContactPage;
